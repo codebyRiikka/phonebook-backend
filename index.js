8c2ab0f576889202ff9fa4a17aac392b354e84cb
Riikka-KaoPT23TIEd <rixuuu95@gmail.com>
2024-08-21 13:22:58 +0300

    Tasks 3.1-3.6, get, delete, post -persons feature and the overwiew how many contacts the phonebook contains.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -26,6 +28,60 @@ let persons = [
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const countPersons = persons.length
+    const currentDate = new Date()
+    const response = 
+    `<p>Phonebook has info for ${countPersons} people</p>
+    <p>${currentDate}</p>`
+
+    res.send(response)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const pNumber = persons.find(person => person.id === id)
+
+    if (pNumber) {
+        res.json(pNumber)
+    } else {
+        res.status(404).json({ error: 'Person not found' })
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    //Is name and number already given -check
+    if (!body.name ||!body.number) {
+        return res.status(400).json({ error: 'Name or number is missing' })
+    }
+
+    //Does name already exists -check
+    const nameExists = persons.some(person => person.name === body.name)
+    if (nameExists) {
+        return res.status(400).json({ error: 'Name already exists, try another' })
+    }
+
+    //Creating a new person
+    const newPerson = {
+        id: Math.floor(Math.random() * 10000), 
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(newPerson)
+
+    res.json(newPerson)
+
 })
 
 const PORT = 3001
