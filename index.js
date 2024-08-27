7fa3be59107dde766c75f9772cc375278e2cc85e
Riikka-KaoPT23TIEd <rixuuu95@gmail.com>
2024-08-27 13:44:08 +0300

    Trying to connect front-end and back-end

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'dist')))
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
@@ -117,6 +119,10 @@ app.post('/api/persons', (req, res) => {
 
     res.json(newPerson)
 
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 const PORT = process.env.PORT || 3001
